1e77fdc64a55dd2b8eb5c5cce739da41003f0e1f
xl <xlmysjz@sina.com>
2013-06-28 11:36:47 +0800

    rm

--- PythonWorkSpace/PythonAutoTestPro/src/myunittest/__init__.py
+++
@@ -1,4 +0,0 @@
-#-*-coding:utf-8-*-
-'''
-没有用到这个 模块，请看 script 和 testmyscript
-'''
--- PythonWorkSpace/PythonKaiFa/src/chapter1/__init__.py
+++
@@ -1,7 +0,0 @@
-#-*-coding:utf-8-*-
-#Python开发技术祥解 的书籍
-
-if __name__=="__main__":
-    age=30
-    name="bill"
-    print "%s is %d years old" %(name,age)
